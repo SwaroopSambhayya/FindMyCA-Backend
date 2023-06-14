b34d18ebcc9193d6ef75630622bfa8215226e4c6
Swaroop Sambhayya <swaroop.sambhayya@gmail.com>
2023-06-14 20:37:57 +0530

    Notification changes

--- functions/sendPushOnBook/lib/const.dart
+++ functions/sendPushOnBook/lib/const.dart
@@ -3,4 +3,4 @@ const profileCollectionId = "646c64f3a6d37627f1a3";
 const profilePicBucketId = '6475fbe09efe8ed159af';
 const projectID = '64663e222959a2ffa6cd';
 const appWriteBaseURl = 'https://cloud.appwrite.io/v1';
-const notificationCollectionId = "64871d83dac915366753";
+const notificationCollectionId = "64899a4f296ac881844b";
